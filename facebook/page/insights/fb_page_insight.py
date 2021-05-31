e6e681b23d8395d76fe817ea0ecde6df85d4723c
Grimmer <grimmer0125@gmail.com>
2021-05-31 22:30:40 +0800

    Add get_recent_posts, get_post_info, and get_post_info_detail

--- facebook/page/insights/fb_page_insight.py
+++ facebook/page/insights/fb_page_insight.py
@@ -1,5 +1,5 @@
-from typing import List, Optional
-from pydantic import BaseModel
+from typing import List, Optional, Union
+from pydantic import BaseModel, Field
 from enum import Enum, auto
 from dataclasses import dataclass, field
 
@@ -53,15 +53,32 @@ class Key(Enum):
     access_token = auto()
 
 
-class Metric(Enum):
+class PostDetailMetric(Enum):
+    post_clicks_by_type = auto()
+    post_activity_by_action_type = auto()
+    post_reactions_like_total = auto()
+    post_reactions_love_total = auto()
+    post_reactions_wow_total = auto()
+    post_reactions_haha_total = auto()
+    post_negative_feedback_by_type_unique = auto()  # not sure web uses it or below
+    post_negative_feedback_by_type = auto()
+
+
+class PostMetric(Enum):
+    post_impressions_organic_unique = auto()
+    post_clicks = auto()
+    post_activity = auto()
+
+
+class PageMetric(Enum):
     page_total_actions = auto()
     page_views_total = auto()
-    page_fan_adds_unique = auto()
+    page_fan_adds_unique = auto()  # not sure web use this or below
+    page_fan_adds = auto()
     page_post_engagements = auto()
     page_video_views = auto()
     page_daily_follows_unique = auto()
     page_impressions_organic_unique = auto()
-    page_fan_adds = auto()
     page_daily_follows = auto()
 
 
@@ -74,9 +91,27 @@ class Category(BaseModel):
     name: str
 
 
+class PostClickValue(BaseModel):
+
+    other_clicks: int = Field(alias='other clicks')
+    photo_view: int = Field(alias='photo view')
+    link_clicks: int = Field(alias='link clicks')
+
+
+class PostActivityValue(BaseModel):
+    share: int
+    like: int
+    comment: int
+
+
 class InsightsValue(BaseModel):
-    value: str
-    end_time: str
+    # TODO: how to handle union?
+    value: Union[int, PostClickValue, PostActivityValue,  dict]  #
+    # if period is lifetime, end_time will not appear here
+    end_time: Optional[str]
+
+    # -post_negative_feedback_by_type_unique/post_negative_feedback_by_type
+    # value: {}
 
 
 class InsightsResult(BaseModel):
@@ -93,10 +128,6 @@ class InsightsCursors(BaseModel):
     # get_page_insights
     previous: str  # similar query but add since & until
     next: str
-
-
-# class InsightsPaging(BaseModel):
-#     cursors: InsightsCursors
 
 
 class InsightsResponse(BaseModel):
@@ -126,6 +157,28 @@ class AccountPaging(BaseModel):
 class AccountResponse(BaseModel):
     data: List[AccountResult]
     paging: AccountPaging
+
+
+class PostsResult(BaseModel):
+    created_time: str
+    message: Optional[str]  # either message or story
+    story: Optional[str]  # "story": "PyCon Taiwan 更新了封面相片。",
+    id: str
+
+
+class PostsPaging(BaseModel):
+    cursors: AccountCursors
+    next: str
+
+
+class PostsResponse(BaseModel):
+    data: List[PostsResult]
+    paging: PostsPaging
+
+
+# class PostInfoResponse(BaseModel):
+#     data: List[InsightsResult]
+#     paging: InsightsCursors
 
 
 class LongLivedResponse(BaseModel):
@@ -180,7 +233,7 @@ class FBPageInsight:
         return ""
 
     # TODO: add since/until (e.g. since=1620802800&until=1620975600)
-    def compose_page_insights_request(self, token, object_id, endpoint, param_dict: dict[str, str]):
+    def compose_page_insights_request(self, token, object_id, endpoint, param_dict: dict[str, str] = {}):
         params = self.convert_para_dict(param_dict)
         url = f'{self.api_url}/{object_id}/{endpoint}?access_token={token}{params}'
         r = requests.get(url)
@@ -193,20 +246,23 @@ class FBPageInsight:
             params += f'&{key}={value}'
         return params
 
-    def convert_metric_list(self, metric_list: List[Metric]):
+    def convert_metric_list(self, metric_list: List[PageMetric]):
         metric_value = ''
-        for metric in metric_list:
-            metric_value += ","+metric.name
+        for i in range(0, len(metric_list)):
+            metric = metric_list[i]
+            if i == 0:
+                metric_value += metric.name
+            else:
+                metric_value += ","+metric.name
         return metric_value
 
-    def get_page_insights(self, page_id, metric_list: List[Metric] = [], date_preset: DataPresent = DataPresent.yesterday, period: Period = Period.week):
+    def get_page_insights(self, page_id, user_defined_metric_list: List[PageMetric] = [], date_preset: DataPresent = DataPresent.yesterday, period: Period = Period.week):
 
         # TODO: validate parameters, metric_list: List[Metric] = [] is not clear to use default?
 
-        if len(metric_list) == 0:
-            metric_list = [e for e in Metric]
-
-        metric_value = self.convert_metric_list(metric_list)
+        if len(user_defined_metric_list) == 0:
+            user_defined_metric_list = [e for e in PageMetric]
+        metric_value = self.convert_metric_list(user_defined_metric_list)
 
         page_token = ""
         if self.page_access_token == "":
@@ -216,18 +272,64 @@ class FBPageInsight:
         json_dict = self.compose_page_insights_request(page_token,
                                                        page_id, "insights", {"metric": metric_value, "date_preset": date_preset.name, 'period': period.name})
 
-        # parse it
         resp = InsightsResponse(**json_dict)
         return resp
 
-    def get_posts(self):
-        return
+    def get_recent_posts(self, page_id):
+        # could use page_token or user_access_token
+        page_token = ""
+        if self.page_access_token == "":
+            page_token = self.get_page_tokens(page_id)
+        else:
+            page_token = self.page_access_token
 
-    def get_post_info(self):
-        return
+        json_dict = self.compose_page_insights_request(page_token,
+                                                       page_id, "posts")
 
-    def get_post_info_detailed(self):
-        return
+        resp = PostsResponse(**json_dict)
+        return resp
+
+    def get_post_info(self, post_id, user_defined_metric_list: List[PostMetric] = []):
+
+        page_id = post_id.split('_')[0]
+
+        if len(user_defined_metric_list) == 0:
+            user_defined_metric_list = [e for e in PostMetric]
+        metric_value = self.convert_metric_list(user_defined_metric_list)
+
+        # TODO: refactor this part
+        page_token = ""
+        if self.page_access_token == "":
+            page_token = self.get_page_tokens(page_id)
+        else:
+            page_token = self.page_access_token
+
+        json_dict = self.compose_page_insights_request(page_token,
+                                                       post_id, "insights", {"metric": metric_value})
+        resp = InsightsResponse(**json_dict)
+        return resp
+
+    # TODO: refactor this
+    def get_post_info_detail(self, post_id, user_defined_metric_list: List[PostDetailMetric] = []):
+        page_id = post_id.split('_')[0]
+
+        if len(user_defined_metric_list) == 0:
+            user_defined_metric_list = [e for e in PostDetailMetric]
+        metric_value = self.convert_metric_list(user_defined_metric_list)
+
+        # TODO: refactor this part
+        page_token = ""
+        if self.page_access_token == "":
+            page_token = self.get_page_tokens(page_id)
+        else:
+            page_token = self.page_access_token
+
+        json_dict = self.compose_page_insights_request(page_token,
+                                                       post_id, "insights", {"metric": metric_value})
+
+        # TODO: add union of responseObject?
+        resp = InsightsResponse(**json_dict)
+        return resp
 
     def dummy_test(self):
         return "ok"
